f2d3594730605369eb3f0785edce15bdeadaed20
wuchangyin <wuchangyin@you-qin.com>
2020-10-27 09:42:51 +0800

    1.c文件提交

---
+++ 1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void swap(int *a,int *b)
+{
+	int c=0;
+	c=*a;
+	*a=*b;
+	*b=c;
+}
+int main()
+{
+	char *str = malloc(4*50); 
+	int a = 3,b = 4;
+	printf("a = %d,b = %d\n",a,b);
+	swap(&a,&b);
+	for (int a = 1;  a < 10; ++ a)
+	{
+		printf("a = %d\n",a);
+	}
+	printf("hello world!\n");
+	printf("a = %d,b = %d\n",a,b);
+	free(str);
+}
